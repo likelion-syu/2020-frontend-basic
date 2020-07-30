e19cc5504272f254f4f6feacce2ca6775c68e05e
KyeongSoo Yoo <dev.yoogomja@gmail.com>
2020-07-30 12:19:52 +0900

    프론트엔드 수업 시작하기

--- assets/js/header.js
+++ assets/js/header.js
@@ -2,25 +2,17 @@
 const _header = {
     // UI 조작에 관련된 함수들 
     ui : {
+        // 남은 할일 표기
         getRemains : data => {
-            let cntOfDoneWorks = 0;
-            data.forEach(item=>{ cntOfDoneWorks += item.done ? 1 : 0 });
-
-            const remains = data.length - cntOfDoneWorks;
-
-            document.querySelector(".header .remains b").innerText = remains;
+            
         },
+        // 현재 날짜 표기
         setDate : ()=>{
-            const now = moment();
-            const header = document.querySelector(".header .current");
-            header.querySelector(".date").innerText = now.date();
-            header.querySelector(".day").innerText = now.locale("en").format("ddd");
-            header.querySelector(".month").innerText = now.locale("en").format("MMMM, yyyy");
+            
         }
     },
     // 일반 함수들
     init : data =>{
-        _header.ui.getRemains(data);
-        _header.ui.setDate();
+        
     }
 }
